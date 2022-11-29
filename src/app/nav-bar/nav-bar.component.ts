76a884d3ff85f9a795a75716c351629f7e0ae510
Sergey Dmitranitsa <siarhei.dzmitranitsa@innowise-group.com>
2022-11-30 02:04:37 +0300

    start project

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'angTest-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+
+  public buttonList: string[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.createButtonList();
+  }
+
+  private createButtonList(): void {
+    this.buttonList = ['home', 'form'];
+  }
+
+}
